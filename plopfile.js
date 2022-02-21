3b0e546843d675963f2781e03c9aadbad6af576a
newBuilder365 <772512900@qq.com>
2022-02-21 14:05:48 +0800

    修改了不带台账得流程表单

--- plopfile.js
+++ plopfile.js
@@ -2,6 +2,6 @@ const standingBook = require('./plop-templates/standingBook/plop')
 const wfFormWithoutStandingBook = require('./plop-templates/wfFormWithoutStandingBook/plop')
 
 module.exports = plop => {
-    plop.setGenerator('创建台账1', standingBook),
+    plop.setGenerator('创建台账', standingBook),
     plop.setGenerator('创建不带台账的流程表单', wfFormWithoutStandingBook)
 }
