98d95a1c20d02c3f64ed59d9cc0013dd75a6b228
newBuilder365 <772512900@qq.com>
2022-08-29 09:53:05 +0800

    增加带台账的非流程表单

--- plopfile.js
+++ plopfile.js
@@ -1,9 +1,11 @@
-const standingBook = require('./plop-templates/standingBook/plop')
-const wfFormWithoutStandingBook = require('./plop-templates/wfFormWithoutStandingBook/plop')
-const wfFormWithStandingBook = require('./plop-templates/wfFormWithStandingBook/plop')
+const standingBook = require("./plop-templates/standingBook/plop");
+const wfFormWithoutStandingBook = require("./plop-templates/wfFormWithoutStandingBook/plop");
+const wfFormWithStandingBook = require("./plop-templates/wfFormWithStandingBook/plop");
+const unWFFormWithStandingBook = require("./plop-templates/unWFFormWithStandingBook/plop");
 
-module.exports = plop => {
-    plop.setGenerator('创建台账', standingBook),
-    plop.setGenerator('创建不带台账的流程表单', wfFormWithoutStandingBook),
-    plop.setGenerator('创建带台账的流程表单', wfFormWithStandingBook)
-}
+module.exports = (plop) => {
+    plop.setGenerator("创建台账", standingBook),
+    plop.setGenerator("创建不带台账的流程表单", wfFormWithoutStandingBook),
+    plop.setGenerator("创建带台账的流程表单", wfFormWithStandingBook),
+    plop.setGenerator("创建带台账的非流程表单", unWFFormWithStandingBook)
+};
